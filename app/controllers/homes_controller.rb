1db56283f1f83b8add2c5442c70edc107faf3608
Ubuntu <ubuntu@ip-172-31-24-180.us-east-2.compute.internal>
2019-07-19 22:23:27 +0000

    base

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,49 @@
+class HomesController < ApplicationController
+  def index
+    @users = Post.all
+  end
+
+  def new
+    @user = Post.new
+  end
+
+  def create
+    @user = Post.new
+    # @user.name = params[:name]
+    @user.name = params[:post][:name]
+    # @user.email = params[:email]
+    @user.email = params[:post][:email]
+    # @user.password = params[:password]
+    @user.password = params[:post][:password]
+    @user.save
+    
+    redirect_to '/'
+  end
+
+  def show
+    @user = Post.find(params[:id])
+  end
+
+  def edit
+    @user = Post.find(params[:id])
+  end
+
+  def update
+    @user = Post.find(params[:id])
+    # @user.name = params[:name]
+    @user.name = params[:post][:name]
+    # @user.email = params[:email]
+    @user.email = params[:post][:email]
+    # @user.password = params[:password]
+    @user.password = params[:post][:password]
+    @user.save
+    
+    redirect_to '/'
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    
+    redirect_to '/'
+  end
+end
